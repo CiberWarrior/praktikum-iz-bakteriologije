ff9439027285fb804660718d16b26b08327e3fbb
Renata Horvat <tvoj.email@example.com>
2025-08-01 13:21:55 +0200

    Prvi commit: Dodavanje praktikuma iz bakteriologije s predgovorom i svim materijalima

---
+++ astro.config.mjs
@@ -0,0 +1,44 @@
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+import path from 'path';
+
+export default defineConfig({
+  integrations: [
+    starlight({
+      title: 'Mrežni udžbenik iz bakteriologije',
+      social: [
+        {
+          icon: 'github',
+          label: 'GitHub',
+          href: 'https://github.com/withastro/starlight',
+        },
+      ],
+      sidebar: [
+        {
+          label: 'Predgovor',
+          items: [{ label: 'Predgovor', slug: 'predgovor' }],
+        },
+        {
+          label: 'Uvod',
+          items: [{ label: 'Dobrodošli', slug: '' }],
+        },
+        {
+          label: 'Poglavlja',
+          items: [{ label: 'Bakteriološki praktikum', slug: 'poglavlje-1' }],
+        },
+        {
+          label: 'Vodiči',
+          items: [{ label: 'Primjer vodiča', slug: 'guides/example' }],
+        },
+      ],
+      customCss: ['@/styles/global.css'],
+    }),
+  ],
+  vite: {
+    resolve: {
+      alias: {
+        '@': path.resolve('./src'),
+      },
+    },
+  },
+});
